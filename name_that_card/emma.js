18307a4e6f79cbdfd14867f9fb4173e68d4d9c35
Daniel Capo <danielccapo@gmail.com>
2016-08-30 16:23:54 -0700

    prep name_that_card for emma

--- name_that_card/emma.js
+++ name_that_card/emma.js
@@ -5,42 +5,39 @@ Array.prototype.randomElement = function() {
 class Deck
 {
     constructor() {
-        this.cards = [];
-        for (var i = 0; i < 51; i++) {
-            this.cards.push(new Card(i));
-        }
+        // Write me!
     }
 
     dealCard() {
-        return this.cards.randomElement();
+        // Write me!
+        return new Card(0);
     }
 }
 
 class Card
 {
     constructor(cardIndex) {
-        this.index = cardIndex;
+        // Write me!
     }
 
     getSuit() {
         let suits = ['Clubs', 'Spades', 'Diamonds', 'Hearts'];
-        let suit = Math.floor(this.index / 13);
-        return suits[suit];
+        // Write me!
     }
 
     getRank() {
         let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
                      'Jack', 'Queen', 'King', 'Ace'];
-        let rank = this.index % 13;
-        return ranks[rank];
+        // Write me!
     }
 
     getIconFilename() {
-        let rankPart = this.getRank() === '10' ? '10' : this.getRank().charAt(0);
-        return rankPart + this.getSuit().charAt(0) + '.png';
+        // Write me!
+        return 'henry_farts.png';
     }
 
     toString() {
-        return this.getRank() + ' of ' + this.getSuit();
+        // Write me!
+        return 'henry farts';
     }
 }
